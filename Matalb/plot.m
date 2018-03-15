4605ad16d8e1cc3b749b1494df9013ff86a37704
ChrisKyrie <36178701+ChrisKyrie@users.noreply.github.com>
2018-03-15 11:10:40 +0800

    Create plot.m

---
+++ Matalb/plot.m
@@ -0,0 +1,170 @@
+vehicleWidth  = 2.5;
+vehicleLength = 3.5;
+
+x = [vehicleWidth/2 vehicleWidth/2];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2 -vehicleWidth/2];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+0.5 vehicleWidth/2+0.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-0.5 -vehicleWidth/2-0.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+3.5 vehicleWidth/2+3.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-3.5 -vehicleWidth/2-3.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 vehicleWidth/2+6.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength vehicleLength];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength*0.9 vehicleLength*0.9];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [0 0];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-3 -3];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-10 -10];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-30 -30];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+3.5 vehicleWidth/2+3.5 vehicleWidth/2+0.5 vehicleWidth/2+0.5];
+y = [vehicleLength*0.9 -3 -3 vehicleLength*0.9];
+fill(x,y,'g')
+
+x = [-vehicleWidth/2-3.5 -vehicleWidth/2-3.5 -vehicleWidth/2-0.5 -vehicleWidth/2-0.5];
+y = [vehicleLength*0.9 -3 -3 vehicleLength*0.9];
+fill(x,y,'g')
+
+x = [-vehicleWidth/2 vehicleWidth/2 vehicleWidth/2 -vehicleWidth/2];
+y = [0 0 vehicleLength vehicleLength];
+fill(x,y,'r')
+
+vehicleWidth  = 2.5;
+vehicleLength = 3.5;
+
+x = [vehicleWidth/2 vehicleWidth/2];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2 -vehicleWidth/2];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+0.5 vehicleWidth/2+0.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-0.5 -vehicleWidth/2-0.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+3.5 vehicleWidth/2+3.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-3.5 -vehicleWidth/2-3.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 vehicleWidth/2+6.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [-vehicleWidth/2-6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength+2 -35];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength vehicleLength];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [vehicleLength*0.9 vehicleLength*0.9];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [0 0];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-3 -3];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-10 -10];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+6.5 -vehicleWidth/2-6.5];
+y= [-30 -30];
+plot(x,y,'k');
+hold on
+
+x = [vehicleWidth/2+3.5 vehicleWidth/2+3.5 vehicleWidth/2+0.5 vehicleWidth/2+0.5];
+y = [vehicleLength*0.9 -3 -3 vehicleLength*0.9];
+fill(x,y,'g')
+
+x = [-vehicleWidth/2-3.5 -vehicleWidth/2-3.5 -vehicleWidth/2-0.5 -vehicleWidth/2-0.5];
+y = [vehicleLength*0.9 -3 -3 vehicleLength*0.9];
+fill(x,y,'g')
+
+x = [-vehicleWidth/2 vehicleWidth/2 vehicleWidth/2 -vehicleWidth/2];
+y = [0 0 vehicleLength vehicleLength];
+fill(x,y,'r')
+
